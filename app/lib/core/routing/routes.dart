ef85ae8e730d91c7444cfe20c4942dabe308401e
Fabian Freund <github@fabi.online>
2024-06-03 01:03:19 +0200

    disable routing redirect

--- app/lib/core/routing/routes.dart
+++ app/lib/core/routing/routes.dart
@@ -1,11 +1,9 @@
 import 'package:bang_navigator/core/routing/dialog_page.dart';
 import 'package:bang_navigator/features/about/presentation/screens/about.dart';
 import 'package:bang_navigator/features/search_browser/presentation/screens/browser.dart';
-import 'package:bang_navigator/features/settings/data/repositories/settings_repository.dart';
 import 'package:bang_navigator/features/settings/presentation/screens/settings.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 part 'routes.g.dart';
 
@@ -22,19 +20,6 @@ part 'routes.g.dart';
 )
 class KagiRoute extends GoRouteData {
   KagiRoute();
-
-  @override
-  Future<String?> redirect(BuildContext context, GoRouterState state) async {
-    final hasKagiSession = await ProviderScope.containerOf(context)
-        .read(settingsRepositoryProvider.future)
-        .then((value) => value.kagiSession?.isNotEmpty ?? false);
-
-    if (!hasKagiSession) {
-      return SettingsRoute().location;
-    }
-
-    return null;
-  }
 
   @override
   Widget build(BuildContext context, GoRouterState state) {
